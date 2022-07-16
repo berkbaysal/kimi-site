99b5ff24aaa733fb6cc92b96d5c09b77b775ed39
berkbaysal <george@do.com>
2022-07-16 13:06:27 +0300

    improved calculations in js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,93 @@
 import './style.scss';
 import { Tooltip, Toast, Popover } from 'bootstrap';
-import {camelCase} from 'lodash';
+import { camelCase } from 'lodash';
 
-function fetchKimiQuote(){
-    let quote;
-     let response = fetch("https://kimiquotes.herokuapp.com/quote")
-     .then((response) => response.json())
-     .then((data) => {
-        console.log(data);
-        quote = data.quote;
-        document.getElementById("quote-box").innerHTML = quote;
-     });
-     
+function fetchKimiQuote() {
+   let quote;
+   let response = fetch("https://kimiquotes.herokuapp.com/quote")
+      .then((response) => response.json())
+      .then((data) => {
+         console.log(data);
+         quote = data.quote;
+         document.getElementById("quote-box").innerHTML = quote;
+      });
+
 }
-function swapQuote(){
-   const quoteBox = document.getElementById("quote-box");
+function swapQuote() {
+   const quoteBox = document.getElementById("quote-box-container");
    quoteBox.classList.add("hidden");
-   setTimeout(()=>fetchKimiQuote(),1100);
-   setTimeout(()=>{quoteBox.classList.add("visible");quoteBox.classList.remove("hidden");},1300);
+   setTimeout(() => {fetchKimiQuote(); swapQuoteSide()}, 1100);
+   setTimeout(() => { quoteBox.classList.add("visible"); quoteBox.classList.remove("hidden"); }, 1300);
 }
-function setHeroHeight(){
+function setHeroHeight() {
    const navbarHeight = document.querySelector(".navbar").offsetHeight;
    const pageHeight = document.querySelector(".land-container").offsetHeight;
    const heroImage = document.getElementById("hero-image-container");
-   heroImage.style.height = (pageHeight-navbarHeight) + "px";
+   heroImage.style.height = (pageHeight - navbarHeight) + "px";
 }
 
-function calcHeroImageDimensions(){
+function calcPageDimensions() {
    const heroImage = document.getElementById("hero-image");
-   const currentWidth = heroImage.offsetWidth;
-   const currentHeight = heroImage.offsetHeight;
-   return [currentWidth,currentHeight];
+   
+   const pageDimensions = {};
+   pageDimensions.heroImage = {};
+   pageDimensions.document = {};
+   pageDimensions.positioning = {};
+   pageDimensions.heroImage.width = heroImage.offsetWidth;
+   pageDimensions.heroImage.height = heroImage.offsetWidth;
+   pageDimensions.document.width = document.querySelector("body").offsetWidth;
+   pageDimensions.document.height = document.querySelector("body").offsetHeight;
+   pageDimensions.document.aspectRatio = pageDimensions.document.width/pageDimensions.document.height;
+   if (pageDimensions.document.aspectRatio > 1.5){
+      pageDimensions.positioning.widthFactor = pageDimensions.heroImage.width/2;
+   }
+   else{
+      pageDimensions.positioning.widthFactor = pageDimensions.heroImage.width/3;
+   }
+
+   return pageDimensions;
 }
 
-function setQuoteBoxPositionAndSize(){
-   const [imageWidth,imageHeight] = calcHeroImageDimensions();
+function setQuoteBoxPositionAndSize() {
    const quoteBox = document.querySelector(".floating-quote");
-   const documentWidth = document.querySelector("body").offsetWidth;
-   const documentHeight = document.querySelector("body").offsetHeight;
+   const pageDimensions = calcPageDimensions();
 
-   quoteBox.style.width = ((documentWidth/2) - (imageWidth/4)*2) + "px";
-   quoteBox.style.left = ((documentWidth/2) - (imageWidth/3)) -quoteBox.offsetWidth + "px";
-   quoteBox.style.top = (imageHeight/4) +"px";
+   quoteBox.style.width = ((pageDimensions.document.width / 2) - (pageDimensions.heroImage.width / 4) * 2) + "px";
+   quoteBox.style.top = (pageDimensions.heroImage.height / 4) + "px";
+   if (quoteBox.style.left == "" && quoteBox.style.right == ""){
+      quoteBox.style.left = ((pageDimensions.document.width / 2) - pageDimensions.positioning.widthFactor) - quoteBox.offsetWidth + "px";
+   }
+   else if (quoteBox.style.left == "") {
+      quoteBox.style.right = ((pageDimensions.document.width / 2) - pageDimensions.positioning.widthFactor) - quoteBox.offsetWidth + "px";
 
-
-   console.log(quoteBox.style.right);
+   }
+   else {
+      quoteBox.style.left = ((pageDimensions.document.width / 2) - pageDimensions.positioning.widthFactor) - quoteBox.offsetWidth + "px";
+   }
 }
+function swapQuoteSide() {
+   const quoteBox = document.querySelector(".floating-quote");
+   const quoteBoxContainer = document.querySelector("#quote-box-container");
+   const pageDimensions = calcPageDimensions();
+
+   if (quoteBox.style.left == "") {
+      quoteBox.style.right = "";
+      quoteBox.style.left = ((pageDimensions.document.width / 2) - pageDimensions.positioning.widthFactor) - quoteBox.offsetWidth + "px";
+      quoteBoxContainer.classList.remove("text-start");
+      quoteBoxContainer.classList.add("text-end");
+   }
+   else {
+      quoteBox.style.left = "";
+      quoteBox.style.right = ((pageDimensions.document.width / 2) - pageDimensions.positioning.widthFactor) - quoteBox.offsetWidth + "px";
+      quoteBoxContainer.classList.add("text-start");
+      quoteBoxContainer.classList.remove("text-end");
+   }
+}
+
 
 setHeroHeight();
 fetchKimiQuote();
 setQuoteBoxPositionAndSize();
-setInterval(swapQuote,10000);
-window.addEventListener("resize",setHeroHeight);
-window.addEventListener("resize",setQuoteBoxPositionAndSize);
+setInterval(swapQuote, 10000);
+window.addEventListener("resize", setHeroHeight);
+window.addEventListener("resize", setQuoteBoxPositionAndSize);
